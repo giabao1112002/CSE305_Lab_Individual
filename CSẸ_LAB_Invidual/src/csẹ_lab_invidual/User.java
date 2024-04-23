28c10697d263b1dbd764b064b1f65086bc0be6ad
giabao1112002 <148312025+giabao1112002@users.noreply.github.com>
2024-04-23 14:18:05 +0700

    a

---
+++ CSẸ_LAB_Invidual/src/csẹ_lab_invidual/User.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package csẹ_lab_invidual;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DELL
+ */
+public class User {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        
+    }
+    
+}
