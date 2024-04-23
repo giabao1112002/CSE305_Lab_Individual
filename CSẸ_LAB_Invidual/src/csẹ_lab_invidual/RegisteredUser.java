f87daccbb4201f485a2b76e5c7de0bd1749518a5
DuyNamCSE <167846477+DuyNamCSE@users.noreply.github.com>
2024-04-23 15:20:02 +0700

    RegisteredUser
    N/a

---
+++ CSẸ_LAB_Invidual/src/csẹ_lab_invidual/RegisteredUser.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package csẹ_lab_invidual;
+
+import java.util.*;
+
+/**
+ *
+ * @author kelyt
+ */
+public class RegisteredUser {
+
+    private static List<UserInfomation> listOfUser = new ArrayList<>();
+
+    public void addUser() {
+    }
+
+    public void removeUSer() {
+    }
+}
