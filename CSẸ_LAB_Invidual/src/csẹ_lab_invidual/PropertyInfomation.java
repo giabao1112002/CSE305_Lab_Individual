77da9a0426c1720182a0c229a59b1df0d0611326
giabao1112002 <148312025+giabao1112002@users.noreply.github.com>
2024-04-23 14:33:39 +0700

    update Property

--- CSẸ_LAB_Invidual/src/csẹ_lab_invidual/PropertyInfomation.java
+++ CSẸ_LAB_Invidual/src/csẹ_lab_invidual/PropertyInfomation.java
@@ -9,5 +9,63 @@ package csẹ_lab_invidual;
  * @author DELL
  */
 public class PropertyInfomation {
-    
-}
+
+    public static void main(String[] args) {
+        Property property = new Property(1, "Example Property", 123);
+        System.out.println(property);
+
+    }
+
+    public static class Property {
+
+        private int propertyID;
+        private String description;
+        private int propertyOwnerId;
+
+        // Constructor
+        public Property(int propertyID, String description, int propertyOwnerId) {
+            this.propertyID = propertyID;
+            this.description = description;
+            this.propertyOwnerId = propertyOwnerId;
+        }
+
+        // Getter for PropertyID
+        public int getPropertyID() {
+            return propertyID;
+        }
+
+        // Setter for PropertyID
+        public void setPropertyID(int propertyID) {
+            this.propertyID = propertyID;
+        }
+
+        // Getter for Description
+        public String getDescription() {
+            return description;
+        }
+
+        // Setter for Description
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        // Getter for PropertyOwnerId
+        public int getPropertyOwnerId() {
+            return propertyOwnerId;
+        }
+
+        // Setter for PropertyOwnerId
+        public void setPropertyOwnerId(int propertyOwnerId) {
+            this.propertyOwnerId = propertyOwnerId;
+        }
+
+        // toString method to represent the object as a string
+        @Override
+        public String toString() {
+            return "Property{"
+                    + "propertyID=" + propertyID
+                    + ", description='" + description + '\''
+                    + ", propertyOwnerId=" + propertyOwnerId
+                    + '}';
+        }
+    }
