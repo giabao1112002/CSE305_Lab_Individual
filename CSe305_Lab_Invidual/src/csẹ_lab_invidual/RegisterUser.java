6e99e6deb77a3641176d26129bf358bb856c050e
giabao1112002 <148312025+giabao1112002@users.noreply.github.com>
2024-05-28 14:17:40 +0700

    update

--- CSe305_Lab_Invidual/src/csẹ_lab_invidual/RegisterUser.java
+++ CSe305_Lab_Invidual/src/csẹ_lab_invidual/RegisterUser.java
@@ -2,29 +2,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package rental_management;
-    import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
+
 /**
  *
  * @author DELL
  */
 public class RegisterUser {
 
+    public class RegisteredUser {
+        private List<User> listOfUsers;
 
-public class RegisteredUser {
-    private List<User> listOfUsers;
+        public RegisteredUser() {
+            this.listOfUsers = new ArrayList<>();
+        }
 
-    public RegisteredUser() {
-        this.listOfUsers = new ArrayList<>();
+        public void addUser(User user) {
+            this.listOfUsers.add(user);
+        }
+
+        public void removeUser(User user) {
+            this.listOfUsers.remove(user);
+        }
     }
-
-    public void addUser(User user) {
-        this.listOfUsers.add(user);
-    }
-
-    public void removeUser(User user) {
-        this.listOfUsers.remove(user);
-    }
-}
 }
